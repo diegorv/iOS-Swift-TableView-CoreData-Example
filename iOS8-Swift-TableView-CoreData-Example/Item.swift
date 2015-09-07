64775d14a053915f22c9dc4554e243c74c052cac
Diego R. V <eu@diegorv.com>
2015-09-07 14:18:45 -0300

    Fix code indentation :)

--- iOS8-Swift-TableView-CoreData-Example/Item.swift
+++ iOS8-Swift-TableView-CoreData-Example/Item.swift
@@ -11,72 +11,72 @@ import CoreData
 
 // MODEL
 class Item: NSManagedObject {
-	
-	@NSManaged var name: String
-	
-	/// Function to initialize a new Item
-	convenience init(name: String, inManagedObjectContext managedObjectContext: NSManagedObjectContext) {
-		let entity = NSEntityDescription.entityForName("Item", inManagedObjectContext: managedObjectContext)!
-		self.init(entity: entity, insertIntoManagedObjectContext: managedObjectContext)
-		self.name = name
-	}
-	
-	/// Function to get all CoreData values
-	///
-	/// :param: managedObjectContext CoreData Connection
-	///
-	class func fetchAll(managedObjectContext: NSManagedObjectContext) -> [Item] {
-		let listagemCoreData             = NSFetchRequest(entityName: "Item")
-		
-		// Sort alphabetical by field "name"
-		let orderByName                  = NSSortDescriptor(key: "name", ascending: true, selector: "caseInsensitiveCompare:")
-		listagemCoreData.sortDescriptors = [orderByName]
-		
-		// Get items from CoreData
-		return managedObjectContext.executeFetchRequest(listagemCoreData, error: nil) as? [Item] ?? []
-	}
-	
-	/// Function to search item by name
-	///
-	/// :param: name Item name
-	/// :param: managedObjectContext CoreData Connection
-	///
-	class func search(name: String, inManagedObjectContext managedObjectContext: NSManagedObjectContext) -> Item? {
-		let fetchRequest       = NSFetchRequest(entityName: "Item")
-		fetchRequest.predicate = NSPredicate(format: "name = %@", name)
-		
-		let result						 = managedObjectContext.executeFetchRequest(fetchRequest, error: nil) as? [Item]
-		return result?.first
-	}
-	
-	/// Function to check duplicate item
-	///
-	/// :param: name Item name
-	/// :param: managedObjectContext CoreData Connection
-	///
-	class func checkDuplicate(name: String, inManagedObjectContext managedObjectContext: NSManagedObjectContext) -> Bool {
-		return search(name, inManagedObjectContext: managedObjectContext) != nil
-	}
 
-	/// Function to delete a item
-	///
-	/// :param: managedObjectContext CoreData Connection
-	///
-	func destroy(managedObjectContext: NSManagedObjectContext) {
-		managedObjectContext.deleteObject(self)
-	}
-	
-	/// Function to save CoreData values
-	///
-	/// :param: managedObjectContext CoreData Connection
-	///
-	func save(managedObjectContext: NSManagedObjectContext) {
-		var error: NSErrorPointer = nil
-		
-		if(managedObjectContext.save(error)) {
-			if (error != nil) {
-				println("Error on save: \(error.debugDescription)")
-			}
-		}
-	}
+  @NSManaged var name: String
+
+  /// Function to initialize a new Item
+  convenience init(name: String, inManagedObjectContext managedObjectContext: NSManagedObjectContext) {
+    let entity = NSEntityDescription.entityForName("Item", inManagedObjectContext: managedObjectContext)!
+    self.init(entity: entity, insertIntoManagedObjectContext: managedObjectContext)
+    self.name = name
+  }
+
+  /// Function to get all CoreData values
+  ///
+  /// :param: managedObjectContext CoreData Connection
+  ///
+  class func fetchAll(managedObjectContext: NSManagedObjectContext) -> [Item] {
+    let listagemCoreData             = NSFetchRequest(entityName: "Item")
+
+    // Sort alphabetical by field "name"
+    let orderByName                  = NSSortDescriptor(key: "name", ascending: true, selector: "caseInsensitiveCompare:")
+    listagemCoreData.sortDescriptors = [orderByName]
+
+    // Get items from CoreData
+    return managedObjectContext.executeFetchRequest(listagemCoreData, error: nil) as? [Item] ?? []
+  }
+
+  /// Function to search item by name
+  ///
+  /// :param: name Item name
+  /// :param: managedObjectContext CoreData Connection
+  ///
+  class func search(name: String, inManagedObjectContext managedObjectContext: NSManagedObjectContext) -> Item? {
+    let fetchRequest       = NSFetchRequest(entityName: "Item")
+    fetchRequest.predicate = NSPredicate(format: "name = %@", name)
+
+    let result             = managedObjectContext.executeFetchRequest(fetchRequest, error: nil) as? [Item]
+    return result?.first
+  }
+
+  /// Function to check duplicate item
+  ///
+  /// :param: name Item name
+  /// :param: managedObjectContext CoreData Connection
+  ///
+  class func checkDuplicate(name: String, inManagedObjectContext managedObjectContext: NSManagedObjectContext) -> Bool {
+    return search(name, inManagedObjectContext: managedObjectContext) != nil
+  }
+
+  /// Function to delete a item
+  ///
+  /// :param: managedObjectContext CoreData Connection
+  ///
+  func destroy(managedObjectContext: NSManagedObjectContext) {
+    managedObjectContext.deleteObject(self)
+  }
+
+  /// Function to save CoreData values
+  ///
+  /// :param: managedObjectContext CoreData Connection
+  ///
+  func save(managedObjectContext: NSManagedObjectContext) {
+    var error: NSErrorPointer = nil
+
+    if(managedObjectContext.save(error)) {
+      if (error != nil) {
+        println("Error on save: \(error.debugDescription)")
+      }
+    }
+  }
 }
--- iOS8-Swift-TableView-CoreData-Example/ViewController.swift
+++ iOS8-Swift-TableView-CoreData-Example/ViewController.swift
@@ -9,197 +9,197 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource {
-	
-	// MARK: - Variables
-	
-	/// CoreData connection variable
-	let coreDataDB = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext!
-	
-	/// TableView array data
-	var items      = [Item]()
-	
-	// MARK: - Outlets
-	
-	// TableView Outlet
-	@IBOutlet weak var tableView: UITableView!
 
-	// MARK: - View Lifecycle
-	
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		
-		// NavigationController title
-		title = "My item list"
-		tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
-  
-		// Load CoreData data
-		items = Item.fetchAll(coreDataDB)
-	}
-	
-	// MARK: - Actions
-	
-	/// TableView add button
-	@IBAction func addButton(sender: UIBarButtonItem) {
-		var addFormAlert    = UIAlertController(title: "New item", message: "Enter a name for the item", preferredStyle: .Alert)
-		let saveButton			= UIAlertAction    (title: "Save", style: .Default) { (action: UIAlertAction!) -> Void in
-			let nameTextField = (addFormAlert.textFields![0] as! UITextField).text
-			
-			// Check if name is empty
-			if (nameTextField.isEmpty) {
-				self.alertError("Unable to save", msg: "Item name can't be blank.")
-			}
-			// Check duplicate item name
-			else if (Item.checkDuplicate(nameTextField, inManagedObjectContext: self.coreDataDB)) {
-				self.alertError("Unable to save", msg: "There is already an item with name: \(nameTextField).")
-			}
-			// Save data
-			else {
-				// Use class "Item" to create a new CoreData object
-				var newItem = Item(name: nameTextField, inManagedObjectContext: self.coreDataDB)
-				
-				// Add item to array
-				self.items.append(newItem)
-				
-				// CoreData save
-				newItem.save(self.coreDataDB)
-				
-				// Reload Coredata data
-				self.items = Item.fetchAll(self.coreDataDB)
-				
-				// Reload TableView
-				self.tableView.reloadData()
-			}
-		}
-		
-		// No action, no need handler
-		let cancelButton = UIAlertAction(title: "Cancel", style: .Destructive, handler: nil)
+  // MARK: - Variables
 
-		// Show textField in alert
-		addFormAlert.addTextFieldWithConfigurationHandler { textField in
-			textField.placeholder = "Item name"
-		}
-		
-		addFormAlert.addAction(saveButton)
-		addFormAlert.addAction(cancelButton)
-		
-		presentViewController(addFormAlert, animated: true, completion: nil)
-	}
-	
-	// MARK: - UITableViewDataSource
-	
-	// TableView rows
-	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return items.count
-	}
-	
-	// Show data in TableView row
-	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-		let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! UITableViewCell
-		
-		cell.textLabel?.text = self.items[indexPath.row].name
-		return cell
-	}
-	
-	// Allow edit cell in TableView
-	func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-		return true
-	}
-	
-	// Allow actions on cell swipe in TableView
-	func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-	}
-	
-	// Actions on cell swipe in TableView
-	func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [AnyObject]? {
-		var editSwipeButton = UITableViewRowAction(style: .Default, title: "Edit") { (action, indexPath) in
-			let currentName = self.items[indexPath.row].name
-			
-			var alertEdit   = UIAlertController(title: "Editing item", message: "Change item name", preferredStyle: .Alert)
-			let saveButton	= UIAlertAction(title: "Save", style: .Default) { (action) in
-				let newItemName = (alertEdit.textFields![0] as! UITextField).text
-				
-				// Check if name is empty
-				if (newItemName.isEmpty) {
-					self.alertError("Unable to save", msg: "Item name can't be blank.")
-				}
-				// Current Name equal New Name
-				else if (currentName == newItemName) {
-					true // do nothing
-				}
-				// Check duplicate item name
-				else if (Item.checkDuplicate(newItemName, inManagedObjectContext: self.coreDataDB)) {
-					self.alertError("Unable to save", msg: "There is already an item with name: \(newItemName).")
-				}
-				// Save CoreData
-				else {
-					// Update item name
-					let item = Item.search(currentName, inManagedObjectContext: self.coreDataDB)
-					item?.name = newItemName
-					item?.save(self.coreDataDB)
-					
-					// Reload Coredata data
-					self.items = Item.fetchAll(self.coreDataDB)
-					
-					// Reload TableView
-					self.tableView.reloadData()
-				}
-				
-				// Close cell buttons
-				self.tableView.setEditing(false, animated: false)
-			}
-			
-			let cancelButton = UIAlertAction(title: "Cancel", style: .Destructive, handler: nil)
-			
-			// Show textField in alert with current name value
-			alertEdit.addTextFieldWithConfigurationHandler { textField in
-				textField.text = currentName
-			}
-			
-			alertEdit.addAction(cancelButton)
-			alertEdit.addAction(saveButton)
-			
-			self.presentViewController(alertEdit, animated: true, completion: nil)
-		}
-		
-		var deleteSwipteButton = UITableViewRowAction(style: .Normal, title: "Delete") { (action, indexPath) in
-			// Find item
-			let itemDelete = self.items[indexPath.row]
-			
-			// Delete item in CoreData
-			itemDelete.destroy(self.coreDataDB)
-			
-			// Save item
-			itemDelete.save(self.coreDataDB)
+  /// CoreData connection variable
+  let coreDataDB = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext!
 
-			// Tableview always load data from "items" array.
-			// If you delete a item from CoreData you need reload array data.
-			self.items = Item.fetchAll(self.coreDataDB)
-			
-			// Remove item from TableView
-			self.tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
-		}
-		
-		editSwipeButton.backgroundColor		 = UIColor.lightGrayColor()
-		deleteSwipteButton.backgroundColor = UIColor.redColor()
-		
-		return [editSwipeButton, deleteSwipteButton]
-	}
-	
-	// MARK: - Helpers
-	
-	/// Show a alert error
-	///
-	/// :param:  title  Title
-	/// :param:  msg    Message
-	///
-	func alertError(title: String, msg: String) {
-		var alert    = UIAlertController(title: title, message: msg, preferredStyle: .Alert)
-		let okButton = UIAlertAction(title: "OK", style: .Cancel, handler: nil)
-		
-		alert.addAction(okButton)
-		
-		self.presentViewController(alert, animated: true, completion: nil)
-	}
+  /// TableView array data
+  var items      = [Item]()
+
+  // MARK: - Outlets
+
+  // TableView Outlet
+  @IBOutlet weak var tableView: UITableView!
+
+  // MARK: - View Lifecycle
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // NavigationController title
+    title = "My item list"
+    tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+
+    // Load CoreData data
+    items = Item.fetchAll(coreDataDB)
+  }
+
+  // MARK: - Actions
+
+  /// TableView add button
+  @IBAction func addButton(sender: UIBarButtonItem) {
+    var addFormAlert    = UIAlertController(title: "New item", message: "Enter a name for the item", preferredStyle: .Alert)
+    let saveButton      = UIAlertAction    (title: "Save", style: .Default) { (action: UIAlertAction!) -> Void in
+      let nameTextField = (addFormAlert.textFields![0] as! UITextField).text
+
+      // Check if name is empty
+      if (nameTextField.isEmpty) {
+        self.alertError("Unable to save", msg: "Item name can't be blank.")
+      }
+      // Check duplicate item name
+      else if (Item.checkDuplicate(nameTextField, inManagedObjectContext: self.coreDataDB)) {
+        self.alertError("Unable to save", msg: "There is already an item with name: \(nameTextField).")
+      }
+      // Save data
+      else {
+        // Use class "Item" to create a new CoreData object
+        var newItem = Item(name: nameTextField, inManagedObjectContext: self.coreDataDB)
+
+        // Add item to array
+        self.items.append(newItem)
+
+        // CoreData save
+        newItem.save(self.coreDataDB)
+
+        // Reload Coredata data
+        self.items = Item.fetchAll(self.coreDataDB)
+
+        // Reload TableView
+        self.tableView.reloadData()
+      }
+    }
+
+    // No action, no need handler
+    let cancelButton = UIAlertAction(title: "Cancel", style: .Destructive, handler: nil)
+
+    // Show textField in alert
+    addFormAlert.addTextFieldWithConfigurationHandler { textField in
+      textField.placeholder = "Item name"
+    }
+
+    addFormAlert.addAction(saveButton)
+    addFormAlert.addAction(cancelButton)
+
+    presentViewController(addFormAlert, animated: true, completion: nil)
+  }
+
+  // MARK: - UITableViewDataSource
+
+  // TableView rows
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return items.count
+  }
+
+  // Show data in TableView row
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! UITableViewCell
+
+    cell.textLabel?.text = self.items[indexPath.row].name
+    return cell
+  }
+
+  // Allow edit cell in TableView
+  func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+    return true
+  }
+
+  // Allow actions on cell swipe in TableView
+  func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+  }
+
+  // Actions on cell swipe in TableView
+  func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [AnyObject]? {
+    var editSwipeButton = UITableViewRowAction(style: .Default, title: "Edit") { (action, indexPath) in
+      let currentName = self.items[indexPath.row].name
+
+      var alertEdit   = UIAlertController(title: "Editing item", message: "Change item name", preferredStyle: .Alert)
+      let saveButton  = UIAlertAction(title: "Save", style: .Default) { (action) in
+        let newItemName = (alertEdit.textFields![0] as! UITextField).text
+
+        // Check if name is empty
+        if (newItemName.isEmpty) {
+          self.alertError("Unable to save", msg: "Item name can't be blank.")
+        }
+        // Current Name equal New Name
+        else if (currentName == newItemName) {
+          true // do nothing
+        }
+        // Check duplicate item name
+        else if (Item.checkDuplicate(newItemName, inManagedObjectContext: self.coreDataDB)) {
+          self.alertError("Unable to save", msg: "There is already an item with name: \(newItemName).")
+        }
+        // Save CoreData
+        else {
+          // Update item name
+          let item = Item.search(currentName, inManagedObjectContext: self.coreDataDB)
+          item?.name = newItemName
+          item?.save(self.coreDataDB)
+
+          // Reload Coredata data
+          self.items = Item.fetchAll(self.coreDataDB)
+
+          // Reload TableView
+          self.tableView.reloadData()
+        }
+
+        // Close cell buttons
+        self.tableView.setEditing(false, animated: false)
+      }
+
+      let cancelButton = UIAlertAction(title: "Cancel", style: .Destructive, handler: nil)
+
+      // Show textField in alert with current name value
+      alertEdit.addTextFieldWithConfigurationHandler { textField in
+        textField.text = currentName
+      }
+
+      alertEdit.addAction(cancelButton)
+      alertEdit.addAction(saveButton)
+
+      self.presentViewController(alertEdit, animated: true, completion: nil)
+    }
+
+    var deleteSwipteButton = UITableViewRowAction(style: .Normal, title: "Delete") { (action, indexPath) in
+      // Find item
+      let itemDelete = self.items[indexPath.row]
+
+      // Delete item in CoreData
+      itemDelete.destroy(self.coreDataDB)
+
+      // Save item
+      itemDelete.save(self.coreDataDB)
+
+      // Tableview always load data from "items" array.
+      // If you delete a item from CoreData you need reload array data.
+      self.items = Item.fetchAll(self.coreDataDB)
+
+      // Remove item from TableView
+      self.tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+    }
+
+    editSwipeButton.backgroundColor    = UIColor.lightGrayColor()
+    deleteSwipteButton.backgroundColor = UIColor.redColor()
+
+    return [editSwipeButton, deleteSwipteButton]
+  }
+
+  // MARK: - Helpers
+
+  /// Show a alert error
+  ///
+  /// :param:  title  Title
+  /// :param:  msg    Message
+  ///
+  func alertError(title: String, msg: String) {
+    var alert    = UIAlertController(title: title, message: msg, preferredStyle: .Alert)
+    let okButton = UIAlertAction(title: "OK", style: .Cancel, handler: nil)
+
+    alert.addAction(okButton)
+
+    self.presentViewController(alert, animated: true, completion: nil)
+  }
 
 }
 
