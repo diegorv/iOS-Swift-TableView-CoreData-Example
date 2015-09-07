a074ed4633b87a7162d6ec694c28eb03b139fab7
Diego R. V <eu@diegorv.com>
2015-09-07 10:11:18 -0300

    Initial Commit

---
+++ iOS8-Swift-TableView-CoreData-ExampleTests/iOS8_Swift_TableView_CoreData_ExampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  iOS8_Swift_TableView_CoreData_ExampleTests.swift
+//  iOS8-Swift-TableView-CoreData-ExampleTests
+//
+//  Created by Diego Rossini Vieira on 9/7/15.
+//  Copyright (c) 2015 Diego Rossini Vieira. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class iOS8_Swift_TableView_CoreData_ExampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
